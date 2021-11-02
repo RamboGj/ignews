1d17fba2400dc663fa71cbc823608f0a6f6802c4
RamboGj <jpramboguanabara@hotmail.com>
2021-11-02 17:34:38 -0300

    final commit

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -2,9 +2,23 @@ import { GetStaticProps } from 'next';
 import Prismic from '@prismicio/client';
 import Head from 'next/head';
 import { getPrismicClient } from '../../services/prismic';
+import Link from 'next/link'
+import { RichText } from 'prismic-dom';
 import styles from './styles.module.scss'
 
-export default function Posts() {
+type Post = {
+    slug: string,
+    title: string,
+    excerpt: string,
+    updatedAt: string,
+}
+
+interface PostsProps {
+   posts: Post[]
+
+}
+
+export default function Posts( { posts }: PostsProps ) {
     return (
         <>
         <Head>
@@ -13,39 +27,45 @@ export default function Posts() {
 
         <main className={styles.container}>
             <div className={styles.posts}>
-                <a href="">
-                    <time>10 de dezembro de 2021</time>
-                    <strong> React News about webhooks</strong>
-                    <p> Welcome to the post about webhooks from react</p>
+                { posts.map(post => (
+                <Link href={`/posts/${post.slug}`}>
+                    <a  key= {post.slug} >
+                        <time>{post.updatedAt}</time>
+                        <strong>{post.title}</strong>
+                    <p>{post.excerpt}</p>
                 </a>
-                <a href="">
-                    <time>10 de dezembro de 2021</time>
-                    <strong> React News about webhooks</strong>
-                    <p> Welcome to the post about webhooks from react</p>
-                </a>
-                <a href="">
-                    <time>10 de dezembro de 2021</time>
-                    <strong> React News about webhooks</strong>
-                    <p> Welcome to the post about webhooks from react</p>
-                </a>
+                </Link>
+                )) }
             </div>
         </main>
-
         </>
     );
 }
 
-export const getSaticProps: GetStaticProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
     const prismic = getPrismicClient()
-    
+
     const response = await prismic.query([
-        Prismic.Predicates.at('document.type', 'post')
+        Prismic.predicates.at('document.type', 'publication')
     ], {
-        fetch: ['post.title', 'post.content'], 
+        fetch: ['publication.title', 'publication.content'],
         pageSize: 100,
-    }) 
+    })
+
+    const posts = response.results.map(post => {
+        return {
+            slug: post.uid,
+            title: RichText.asText(post.data.title),
+            excerpt: post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
+            updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+                day: '2-digit',
+                month: 'long',
+                year: 'numeric',
+            })
+        };
+    })
 
     return {
-        props: {}
-    } 
+        props: { posts }
+    }
 }
